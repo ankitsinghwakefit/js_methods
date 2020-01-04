00c5c7b3901f4a3ea5a532574a8090ed608c8191
ankit <ankitsinghwakefit@gmail.com>
2020-01-04 18:58:32 +0530

    js_methods completed

--- array-methods/2-array.js
+++ array-methods/2-array.js
@@ -9,21 +9,46 @@ var words = [
 ];
 //Write a function findLongestWord that takes an array of words and returns the longest one.
 //If there are 2 with the same length, it should return the first occurrence.
-
-
-
+function findLongestWord(firstword, Secondword){
+  var lengthOfFirst = firstword.length;
+  var lengthOfSecond = Secondword.length;
+  if(lengthOfFirst>lengthOfSecond){
+    return firstword;
+  } else if(lengthOfFirst<lengthOfSecond){
+    return Secondword;
+  } else {
+    return firstword;
+  }
+}
 
 var numbers1 = [6, 12, 1, 18, 13, 16, 2, 1, 8, 10];
 // Create a sumArray function that takes an array of numbers1 as a parameter, and calculate the sum of all its numbers
 // Use reduce method of array
 // Use the above sum and calculate the average.
 
-
+var numbers1 = [6, 12, 1, 18, 13, 16, 2, 1, 8, 10];
+function sumArray(item_price){
+  var total = item_price.reduce(function(sum,item){
+    return sum+item;
+  }
+  ,0);
+  console.log(total);
+  var avg = total/(item_price.length);
+  console.log(avg);
+}
 
 
 var numbers2 = [2, 6, 9, 10, 7, 4, 1, 9];
 //Write a function averageNumbers that receives an array of numbers2 and calculate the average of the numbers
-
+function averageNumbers(item_price){
+  var total = item_price.reduce(function(sum,item){
+    return sum+item;
+  }
+  ,0);
+  console.log("total: "+total);
+  var avg = total/(item_price.length);
+  console.log("Avg: "+avg);
+}
 
 
 
@@ -41,5 +66,10 @@ var words2 = [
 ];
 //Write a function averageWordLength that receives an array of words2 and calculate the average length of the words.
 
-
-
+function averageWordLength(statement){
+  var avg = statement.reduce(function(sum,word){
+    return sum += word.length;
+  },0);
+  var finalAvg = avg/(statement.length);
+  console.log("Avg: "+finalAvg);
+}
--- array-methods/practice.js
+++ array-methods/practice.js
@@ -31,3 +31,28 @@ var cuts = [
 ];
 
 var numbers = [1, 2, 3, 4, 5, 6, 23, 121, 345, 33, 23, 12, 435, 642, 66, 23];
+
+// filter for numbers > 80
+var largenum = numbers.filter(function(num){
+  return num>80;
+});
+
+var largenum = numbers.filter(numb => numb>80);
+console.log(largenum);
+
+var largeNum1 = numbers.filter(value => value>200).map(value1 => value1*5).reduce(function(count,value2){
+  return count+value2;
+},0);;
+console.log(largeNum1);
+
+// printing all the 3 array by using diferent methods.
+
+pizzas.forEach(item => console.log(item));
+cuts.forEach(item => console.log(item));
+numbers.forEach(item => console.log(item));
+
+// removing 2 items from list from begaining.
+
+var shortlist = pizzas.splice(0,2);
+var shortlist = cuts.splice(0,2);
+var shortlist = numbers.splice(0,2);
